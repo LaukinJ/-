d44ebf7eb507a684a733c0c199c68c42a59c13c2
Laukin <1004320638@qq.com>
2019-10-10 16:48:47 +0800

    day3

---
+++ day3/登录注册/be/tmp/foo/routes/sign.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function (req, res, next) {
+    let {
+        username,
+        password
+    } = req.body
+    if (username === '123' && password === '456') {
+        res.json({
+            status: 1,
+            msg: '登录成功'
+        });
+    } else {
+        res.json({
+            status: 0,
+            msg: '登录失败'
+        });
+    }
+})
+module.exports = router;
