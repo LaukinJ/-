175234b7856a968cf89edaf5420bcb3f4f8da5d6
Laukin <1004320638@qq.com>
2019-10-15 14:16:43 +0800

    day3

---
+++ vue/day2/webpack/src/index.js
@@ -0,0 +1,7 @@
+var html = require('./index.html')
+$('body').html(html);
+require('./weui.css');
+require('./index.css');
+import $ from "jquery"
+window.$=$
+window.jquery=$
---
+++ vue/day2/webpack/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // 可以设置为开发模式或者生产模式 development和production
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+
+};
