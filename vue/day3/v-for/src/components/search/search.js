67e9668c2a9c4639c29db704cf450a81a5e47e05
Laukin <1004320638@qq.com>
2019-10-17 14:41:48 +0800

    day4观察着模式进行监听，实现模糊搜索

--- vue/day3/v-for/src/components/search/search.js
+++ vue/day3/v-for/src/components/search/search.js
@@ -1,5 +1,6 @@
 import Vue from 'vue/dist/vue'
 import template from './search.html'
+import observer from '../../tools/observer'
 const search = new Vue({
     el: '#search',
     data: {
@@ -14,6 +15,13 @@ const search = new Vue({
         },
         clear() {
             this.searchText = ''
+        }
+    },
+    watch: {
+        searchText() {
+            // console.log(this.searchText);
+            
+            observer.emit('setSeatchText', this.searchText)
         }
     }
 })
